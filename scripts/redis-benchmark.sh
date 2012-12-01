b91cc1cc211cde0f7e5df5d993fe53d365fdb441
Timothy Fitz <TimothyFitz@gmail.com>
2012-11-30 23:24:58 -0500

    Rerun tests with bugfix for errors. Now 2x slower for GETS/SETS, 4x slower for LRANGE_600 test (decode-heavy).

--- scripts/redis-benchmark.sh
+++ scripts/redis-benchmark.sh
@@ -2,4 +2,4 @@
 echo "Direct redis"
 redis-benchmark -q -p 6379 -c 10 -n 1000 | tee benchmark.redis.txt
 echo "Proxied redis"
-redis-benchmark -q -p 9999 -c 10 -n 100 | tee benchmark.redproxy.txt
+redis-benchmark -q -p 9999 -c 10 -n 1000 | tee benchmark.redproxy.txt
